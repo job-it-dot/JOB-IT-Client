742e5a69de67dab99f8691c9c504de4dab91b106
Daniel <ahribori@gmail.com>
2020-06-13 00:10:30 +0900

    FIXED logo png to svg

--- src/components/common-components/logo/Logo.js
+++ src/components/common-components/logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoImage from '../../../assets/jobit.png';
+import LogoImage from '../../../assets/jobit.svg';
 
 const Logo = () => {
   return (
