fe1749a45f357a56a20fa30f4d894134bb80894a
Jinhyuck Tak <46931292+TakJinhyuck@users.noreply.github.com>
2020-07-07 17:20:37 +0900

    Update Menu.js

--- src/components/common-components/layout/Menu/Menu.js
+++ src/components/common-components/layout/Menu/Menu.js
@@ -1,47 +1,32 @@
-import React from 'react';
-import classNames from 'classnames';
-import classes from './Menu.module.less';
-import { SearchOutlined, BellOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import UserMenu from './UserMenu';
+import CompanyMenu from './CompanyMenu';
 
-const Menu = () => {
-  return (
-    <>
-      <nav className={classes.nav}>
-        <ul className={classes.list}>
-          <li className={classNames(classes.item, classes.active)}>
-            <Link to="/jobs" style={{ color: 'black' }}>
-              탐색
-            </Link>
-          </li>
-          <li className={classes.item}>직군별 연봉</li>
-          <li className={classes.item}>
-            <Link to="/resumeList" style={{ color: 'black' }}>
-              이력서
-            </Link>
-          </li>
-          <li className={classes.item}>추천</li>
-          <li className={classes.item}>이벤트</li>
-        </ul>
-      </nav>
-      <nav className={classes.nav}>
-        <ul className={classes.list}>
-          <li className={classes.item}>
-            <Link to="/signup">회원가입</Link>
-            <Link to="/login">/로그인</Link>
-          </li>
-          <li className={classes.item}>
-            <Link to="/mypage">마이페이지</Link>
-          </li>
-          <li className={classes.item}><Link to="/companymain">기업 서비스</Link></li>
-        </ul>
-      </nav>
-      <aside className={classes.aside}>
-        <SearchOutlined />
-        <BellOutlined />
-      </aside>
-    </>
-  );
-};
+class Menu extends Component {
+  state = {
+    pageNumber: 0,
+  };
+
+  handleChange = () => {
+    this.setState({ pageNumber: 1 });
+    console.log('123');
+  };
+
+  handleClick = () => {
+    this.setState({ pageNumber: 0 });
+    console.log('123');
+  };
+
+  render() {
+    const pageNumber = this.state.pageNumber;
+    let page = null;
+    if (pageNumber === 0) {
+      page = <UserMenu handleChange={this.handleChange} />;
+    } else if (pageNumber === 1) {
+      page = <CompanyMenu handleClick={this.handleClick} />;
+    }
+    return <>{page}</>;
+  }
+}
 
 export default Menu;
